318d1e7aefe2a0efce0dce35a569f40b52df5ef8
deepankar96 <dipankar.sarmah1639@gmail.com>
2020-02-24 21:45:50 +0530

    Q10

--- test.py
+++ test.py
@@ -1,19 +1,10 @@
-def searchMatrix(self, A, B):
-        def binarySearch(arr,l,u,x):
-            mid = (l+u)/2
-            while l<=u:
-                if mid == x:
-                    return 1
-                elif mid > x:
-                    u = mid
-                elif mid < x:
-                    l = mid +1
-            return 0
-            
-        for i in range(len(A[0])):
-            if B>A[0][i]:
-                row = i-1
-                temp = []
-                for j in range(len(A)):
-                    temp.append(A[row][j])
-                return binarySearch(temp,0,len(A),B)
+test = "   "
+'''if len(test)==0:
+            print(0)
+split_arr = test.split(" ")
+if split_arr == None:
+	print(len(test))
+while(len(split_arr[-1])<=0):
+	split_arr.pop()
+print(len(split_arr[-1]))'''
+print(test.split(" ")[-1])
