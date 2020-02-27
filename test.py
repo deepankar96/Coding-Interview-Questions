3f1de3dcca8a15854983d00b2f695aba09b4b5e1
deepankar96 <dipankar.sarmah1639@gmail.com>
2020-02-27 13:44:05 +0530

    Q11

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
-test = "   "
-'''if len(test)==0:
-            print(0)
-split_arr = test.split(" ")
-if split_arr == None:
-	print(len(test))
-while(len(split_arr[-1])<=0):
-	split_arr.pop()
-print(len(split_arr[-1]))'''
-print(test.split(" ")[-1])
+def intersect(A, B):
+        hash_arr = []
+        res = []
+        for i in A:
+            hash_arr.append(hash(i))
+        for i in B:
+            hash_i = hash(i)
+            if hash_i in hash_arr:
+                res.append(i)
+        return set(res)
+A= [1, 3, 8, 10, 13, 13, 16, 16, 16, 18, 21, 23, 24, 31, 31, 31, 33, 35, 35, 37, 37, 38, 40, 41, 43, 47, 47, 48, 48, 52, 52, 53, 53, 55, 56, 60, 60, 61, 61, 63, 63, 64, 66, 67, 67, 68, 69, 71, 80, 80, 80, 80, 80, 80, 81, 85, 87, 87, 88, 89, 90, 94, 95, 97, 98, 98, 100, 101]
+B= [5, 7, 14, 14, 25, 28, 28, 34, 35, 38, 38, 39, 46, 53, 65, 67, 69, 70, 78, 82, 94, 94, 98]
+
+print(intersect(A,B))
